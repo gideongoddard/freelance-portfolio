3c461c83e0a97c66fe28c2b8be1143bc55647d65
Gideon Goddard <goddard.gcw@gmail.com>
2021-01-10 20:29:59 +0000

    Added testimonial component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Gideon from "../components/Gideon/Gideon"
 import Layout from "../components/Layout/Layout"
 import ProjectSummary from "../components/ProjectSummary/ProjectSummary"
 import ServiceSummary from "../components/ServiceSummary/ServiceSummary"
+import Testimonial from "../components/Testimonial/Testimonial"
 // import HomeStyles from "./index.module.css"
 import Title from "../components/Title/Title"
 
@@ -20,6 +21,10 @@ export default function Home() {
       <ServiceSummary />
       <ProjectSummary />
       <Gideon />
+      <Testimonial
+        quote="Gideon is amazing! His services and knowledge have helped my business grow really well. Gideon is patient, kind, helpful and incredibly talented. I cannot recommend him highly enough!"
+        client="Emily Coombes, Kibworth Osteopaths & Pilates"
+      />
     </Layout>
   )
 }
