6f60138a1db5eb37ca46737f68eeb69d0942ff9b
Gideon Goddard <goddard.gcw@gmail.com>
2021-01-10 20:48:19 +0000

    More heading nonsense.

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -4,7 +4,7 @@ import TestimonialStyles from "./Testimonial.module.css"
 export default function Testimonial(props) {
     return (
         <section className={TestimonialStyles.section}>
-            <h2>Don't take my word for it</h2>
+            <h2>Don't take my word for it...</h2>
             <div className={TestimonialStyles.container}>
                 <div className={TestimonialStyles.quote}>
                     <p><em>{props.quote}</em></p>
