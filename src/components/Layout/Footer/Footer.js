c95090c6dabf771871d9a0825372c300701ba65a
Gideon Goddard <goddard.gcw@gmail.com>
2021-01-15 12:43:55 +0000

    Added footer-bottom

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from "react"
+import FooterStyles from "./Footer.module.css"
 
 export default function Footer() {
+    const date = new Date()
+    const year = date.getFullYear()
     return (
-        <footer>
-            
+        <footer className={FooterStyles.footer}>
+            <div className={FooterStyles.footerContent}>
+                <p>Website built using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a></p>
+                <p>{year} &#169; Gideon Goddard <span role="img" aria-label="computer emoji">💻</span> <span role="img" aria-label="sparkle emoji">✨</span></p>
+            </div>
         </footer>
     )
 }
