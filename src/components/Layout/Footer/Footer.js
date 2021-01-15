bf29a12475492a29e63d003156cb02dcc25dcca8
Gideon Goddard <goddard.gcw@gmail.com>
2021-01-15 17:02:28 +0000

    hover effect on service

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     const year = date.getFullYear()
     return (
         <footer className={FooterStyles.footer}>
-            <div className={FooterStyles.footerContent}>
+            <div className={FooterStyles.footerBottom}>
                 <p>Website built using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a></p>
                 <p>{year} &#169; Gideon Goddard <span role="img" aria-label="computer emoji">💻</span> <span role="img" aria-label="sparkle emoji">✨</span></p>
             </div>
