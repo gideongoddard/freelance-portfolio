f1a845a6a62fd8a4448bfded428d44989a73c6f1
Gideon Goddard <goddard.gcw@gmail.com>
2021-01-16 23:06:37 +0000

    Set up header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,17 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import HeaderStyles from "./Header.module.css"
-import { Link } from "gatsby"
+import { graphql, Link, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
 
 export default function Header() {
+    const [navOpen, setNavOpen] = useState(false)
+
     return (
         <header className={HeaderStyles.header}>
-            <nav className={HeaderStyles.nav}>
-                <Link activeStyle={{color: "var(--accent-500)"}} to="/">Home</Link>
-                <Link activeStyle={{color: "var(--accent-500)"}} to="/services">Services</Link>
-                <Link activeStyle={{color: "var(--accent-500)"}} to="/projects">Projects</Link>
-                <Link activeStyle={{color: "var(--accent-500)"}} to="/about">About</Link>
-                <Link activeStyle={{color: "var(--accent-500)"}} to="/hire-me">Hire me</Link>
-            </nav>
+            <div className={HeaderStyles.navContainer}>
+                <Link className={HeaderStyles.logo} to="/">
+                    <StaticQuery
+                        query={graphql`
+                            query {
+                                file(relativePath: {eq: "images/logo.png"}) {
+                                    childImageSharp {
+                                        fixed(width: 80, quality: 72) {
+                                            ...GatsbyImageSharpFixed_noBase64
+                                        }
+                                    }
+                                }
+                            }
+                        `}
+                        render={data => (
+                            <Img fixed={data.file.childImageSharp.fixed} alt="Logo" />
+                        )}
+                    />
+                </Link>
+                <div className={HeaderStyles.toggle} navOpen={navOpen} onClick={() => setNavOpen(!navOpen)}>
+                    <div className={navOpen ? `${HeaderStyles.hamburgerOpen}` : `${HeaderStyles.hamburger}`}></div>
+                </div>
+                <nav className={HeaderStyles.nav} style={{right: navOpen ? "0" : "-100%"}}>
+                    <Link activeStyle={{color: "var(--primary-500)"}} to="/">Home</Link>
+                    <Link activeStyle={{color: "var(--primary-500)"}} to="/services">Services</Link>
+                    <Link activeStyle={{color: "var(--primary-500)"}} to="/projects">Projects</Link>
+                    <Link activeStyle={{color: "var(--primary-500)"}} to="/about">About</Link>
+                    <Link activeStyle={{color: "var(--primary-500)"}} to="/hire-me">Hire me</Link>
+                </nav>
+            </div>
         </header>
     )
 }
