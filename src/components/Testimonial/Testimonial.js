daad0c2eaed202039ceea89eb09745770d439a3d
Gideon Goddard <goddard.gcw@gmail.com>
2021-04-14 11:37:47 +0100

    Styling update for testimonial

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -5,13 +5,13 @@ import * as TestimonialStyles from "./Testimonial.module.css"
 function Testimonial(props) {
     return (
         <section className={TestimonialStyles.section}>
-            <h2>Don't take my word for it...</h2>
+            <h2>Don't just take my word for it...</h2>
             <div className={TestimonialStyles.container}>
                 <div className={TestimonialStyles.quote}>
-                    <p><em>{props.quote}</em></p>
+                    <p>{props.quote}</p>
                 </div>
                 <div className={TestimonialStyles.client}>
-                    <p><strong>- {props.client}</strong></p>
+                    <p>- {props.client}</p>
                 </div>
             </div>
         </section>
