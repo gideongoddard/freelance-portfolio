f563e757cd0a6789c372e3d0c3c83dc11e227649
Gideon Goddard <goddard.gcw@gmail.com>
2021-05-26 15:30:43 +0100

    Changed footer text

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <footer>
             <div className={FooterStyles.footerBottom}>
                 <p>Website built using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a> and hosted on <a href="https://www.netlify.com/" target="_blank" rel="noreferrer noopener">Netlify</a></p>
-                <p>{year} &#169; Gideon Goddard <span role="img" aria-label="computer emoji">💻</span> <span role="img" aria-label="sparkle emoji">✨</span></p>
+                <p>{year} &#169; Gideon Goddard Digital <span role="img" aria-label="computer emoji">💻</span> <span role="img" aria-label="sparkle emoji">✨</span></p>
             </div>
         </footer>
     )
