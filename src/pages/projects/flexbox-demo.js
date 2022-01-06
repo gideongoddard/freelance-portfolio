a2405b0fc1dba3b9a53e43069f67b75fbf1d56d9
Gideon Goddard <goddard.gcw@gmail.com>
2022-01-06 14:11:56 +0000

    Added shrink and basis examples to flexbox demo

--- src/pages/projects/flexbox-demo.js
+++ src/pages/projects/flexbox-demo.js
@@ -18,12 +18,12 @@ function Flexbox() {
                     <section>
                         <h2>An introduction, but not to flexbox.</h2>
                         <p>You know what? I'm gonna spare you the background. There are <em>many</em> other sources with much better descriptions than I could give you of the why, when and how of flexbox's origins and principles. Check out <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/#background" target="_blank" rel="noopener noreferrer">this great guide on CSS-Tricks</a> for starters.</p>
-                        <p>What I want to do is provide a working example of <span style={{letterSpacing: '2px', fontWeight: 700, textDecoration: 'underline', color: 'var(--primary-300)'}}>everything</span> that CSS flexbox can do. So what follows is a visual demonstration of the effect of every possible value of every possible flexbox property. Buckle up!</p>
+                        <p>What I want to do is provide a working example of <em>everything</em> that CSS flexbox can do. So what follows is a visual demonstration of the effect of every value option (within reason) of every possible flexbox property. Buckle up!</p>
                     </section>
                     <section>
-                        <h2>The very basics.</h2>
-                        <p>In this section, I'm going to keep the flexbox properties I use to a minimum, in order to focus on demonstrating what flexbox does by default. For starters, I'm going to keep the only flexbox property used on the flex containers to <code>display: flex;</code> and will focus on how the properties on the flex items within those flex containers affect the layouts.</p>
-                        <h3>What happens by default?</h3>
+                        <h2>Flex items.</h2>
+                        <p>In this section, I'm going to focus on what you can do with the properties that apply to flex items and will ignore adding flexbox properties to flex containers, except of course for adding <code>display: flex;</code> so that we're actually using flexbox at all.</p>
+                        <h3>Initial (default) values.</h3>
                         <p>In this first example, there are no flexbox properties applied to the flex items, so what you see is the default behaviour based on the container having its display property set to 'flex'.</p>
                         <p>The result is that the flex items display as a row, line up at the start of the conainer, do not grow to fill the container and do not wrap. What you will see is that space is allocated to items based on the content within them.</p>
                         <div className={FlexboxStyles.flexContainer}>
@@ -37,7 +37,7 @@ function Flexbox() {
                             </div>
                         </div>
                         <h3>flex-grow</h3>
-                        <p>Here's what happens when the example above sees the flex items have <code>flex-grow: 1;</code> applied to them. You can see that the now grow to fill the container and that they've both grown at the same rate based on their initial widths.</p>
+                        <p>Here's what happens when the example above sees the flex items have <code>flex-grow: 1;</code> applied to them. Unlike the initial value for flex-grow (which is the equivalent of '0'), you can see that they now grow to fill the container and that they've both grown at the same rate based on their initial widths.</p>
                         <div className={FlexboxStyles.flexContainer}>
                             <div className={FlexboxStyles.flexItem} style={{flexGrow: 1}}>
                                 <h4>Flex item 1</h4>
@@ -48,15 +48,39 @@ function Flexbox() {
                                 <p>Lorem ipsum dolor sit amet.</p>
                             </div>
                         </div>
-                        <p>Should we want them to grow at different rates, we can apply different values to the 'flex-grow' properties. In this example, Flex item 2 is given <code>flex-grow: 2;</code> and therefore grows at 2x the rate of Flex item 1.</p>
+                        <p>Should we want them to grow at different rates, we can apply different values to the 'flex-grow' properties. In this example, Flex item 2 is given <code>flex-grow: 3;</code> and therefore grows at 3x the rate of Flex item 1.</p>
                         <div className={FlexboxStyles.flexContainer}>
                             <div className={FlexboxStyles.flexItem} style={{flexGrow: 1}}>
                                 <h4>Flex item 1</h4>
                                 <p>Lorem ipsum.</p>
                             </div>
-                            <div className={FlexboxStyles.flexItem} style={{flexGrow: 2}}>
+                            <div className={FlexboxStyles.flexItem} style={{flexGrow: 3}}>
                                 <h4>Flex item 2</h4>
                                 <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+                        <h3>flex-shrink</h3>
+                        <p>The flex-shrink property controls how a flex item adjusts to going below their initial size. This first example will move straight into the two flex items having different values for this, because the initial value that will be demonstrated in all previous examples is equivalent to '1'. Here, Flex item 1 is assigned <code>flex-shrink: 0;</code> whilst Flex item 2 is assigned <code>flex-shrink: 3;</code> meaning that Flex item 1 does not shrink at all, while Flex item 3 shrinks at 3x the default rate.</p>
+                        <div className={FlexboxStyles.flexContainer}>
+                            <div className={FlexboxStyles.flexItem} style={{flexGrow: 1, flexShrink: 0}}>
+                                <h4>Flex item 1</h4>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                            <div className={FlexboxStyles.flexItem} style={{flexGrow: 1, flexShrink: 3}}>
+                                <h4>Flex item 2</h4>
+                                <p>Esse veniam nostrud pariatur labore Lorem aliquip.</p>
+                            </div>
+                        </div>
+                        <h3>flex-basis</h3>
+                        <p>This sets the initial size of the flex item. Whilst the initial (default) value is 'auto', this example shows the result if Flex item 1 is assigned <code>flex-basis: 0;</code> and Flex item 2 is assigned <code>flex-basis: 60%;</code>. Both of these items have their initial values for flex-grow and flex-shrink.</p>
+                        <div className={FlexboxStyles.flexContainer}>
+                            <div className={FlexboxStyles.flexItem} style={{flexBasis: 0}}>
+                                <h4>Flex item 1</h4>
+                                <p>Lorem ipsum.</p>
+                            </div>
+                            <div className={FlexboxStyles.flexItem} style={{flexBasis: '60%'}}>
+                                <h4>Flex item 2</h4>
+                                <p>Lorem ipsum.</p>
                             </div>
                         </div>
                     </section>
